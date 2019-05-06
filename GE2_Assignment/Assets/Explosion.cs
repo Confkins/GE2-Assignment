ecc1ac197122d0e290a024cda533de8cd55c00a9
Confkins <confkins@gmail.com>
2019-05-07 00:36:50 +0100

    Added Phasers, Ship Destruction

---
+++ GE2_Assignment/Assets/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosion;
+    public Vector3 endScale;
+    public Vector3 inflation = new Vector3(3, 3, 3);
+
+    void Start()
+    {
+        explosion = gameObject;
+        endScale = new Vector3(20.0f, 20.0f, 20.0f);
+        transform.localScale = Vector3.zero;
+    }
+
+    void Update()
+    {
+        transform.localScale += inflation;
+        if (transform.localScale.y > endScale.y)
+        {
+            Destroy(explosion);
+        }
+
+    }
+}
---
+++ GE2_Assignment/Assets/Phasers.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phasers : MonoBehaviour
+{
+    public GameObject phaserPrefab;
+    public GameObject phaser;
+    public bool fire = false;
+    public float targetDistance;
+    public float firingDistance = 40.0f;
+    public float firingArc = 5;
+
+    Transform target;
+
+
+    void Start()
+    {
+        StartCoroutine("PhaserTimer");
+    }
+
+    public IEnumerator PhaserTimer()
+    {
+        while(1==1)
+        {
+            if (fire == true)
+            {
+                firePhaser();
+                yield return new WaitForSeconds(1);
+                Destroy(phaser);
+                yield return new WaitForSeconds(3);
+                fire = false;
+            }
+            yield return null;
+        }
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position + (transform.forward*10), 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.SphereCast(transform.position, firingArc, transform.forward, out hit, firingDistance))
+        {
+            targetDistance = hit.distance;
+            target = hit.transform;
+
+            if (hit.transform.tag == "BorgCube" && fire == false)
+            {
+                fire = true;
+            }
+        }
+
+
+
+    }
+    void firePhaser()
+    {
+        Vector3 pos = Vector3.Lerp(transform.position, target.position, (float)0.5);
+        phaser = Instantiate(phaserPrefab, pos, phaserPrefab.transform.rotation);
+        phaser.transform.SetParent(transform);
+        phaser.transform.localScale = new Vector3(0.1f,targetDistance,0.1f);
+        phaser.transform.LookAt(target);
+        phaser.transform.Rotate(new Vector3(90,0,0),Space.Self);
+    }
+}
